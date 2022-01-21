e9065605cec306fb2b755bafa6fde3fc316e1d03
Dmitrii Chizhov <dimachig@gmail.com>
2022-01-21 14:59:21 +0300

    init: Morse decoder solution

--- src/index.js
+++ src/index.js
@@ -37,10 +37,59 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+/* -------SlashDots decoder----------------- */
+
+function slashDots(str) {
+    let mo = []
+    for(let i in str) {
+     str[i] === '10' ? str[i] = '.' : str[i] === '11' ? str[i] = '-' : str[i] = ' '; 
+     mo.push(str[i])
+    }
+    return mo.join('');
 }
 
+/* ----------DECIMAL FUNCTION--------------- */
+function decimal(str, newSize) {
+    let arr = [];
+    while(str !== '') {
+        arr.push(str.slice(0, newSize));
+        str = str.slice(newSize);
+    }
+    return arr;
+}
+
+/* --------------Decimal decoder-------------- */
+
+function decode(expr) {
+    let decimalArray = decimal(expr, 10);
+    let array = [];
+    let morse = [];
+    let keys = [];
+    let morseKeys = [];
+    let answer = [];
+    let result = ''
+    array = decimalArray.map(letter => {
+        return letter.split('00').join('');
+    })
+    for(let index in array){
+        let check = array[index];
+        morse = decimal(check, 2);
+        keys.push(morse);
+    }
+    for(let i in keys) {
+        let temp = slashDots(keys[i])
+        morseKeys.push(temp);
+    }
+    for(let i in morseKeys){
+        let letter = MORSE_TABLE[morseKeys[i]];
+        if(letter === undefined){
+            letter = ' ';
+        }
+        answer.push(letter);
+        result = answer.join('')
+    }
+    return result;
+}
 module.exports = {
     decode
 }
